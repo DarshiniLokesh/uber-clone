3d10986ed8f2f46c571a73058d78d4d682456ade
DarshiniLokesh <darshini.lokesh22@gmail.com>
2025-03-17 00:07:37 +0530

    feat : implemented auth screen

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView,Text } from 'react-native';
+import { Redirect } from 'expo-router';
+const Home = () => {
+  return (
+    <Redirect href="/(auth)/welcome" />
+  )
+}
+
+
+export default Home;
