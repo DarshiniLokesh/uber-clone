0dcc60b262b8d44929f8ee0d4c8c61b3cbe945a8
DarshiniLokesh <darshini.lokesh22@gmail.com>
2025-03-27 23:13:05 +0530

    included map

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -1,18 +1,31 @@
-import {Text, View} from "react-native"
+import React from "react";
+import { Text, View, TouchableOpacity, Image } from "react-native";
 import { GoogleInputProps } from "@/app/types/type";
-const GoogleTextInput = () => ({
-    icon,
-    initialLocation,
-    containerStyle,
-    textInputBackgroundColor,
-    handlePress,
-    }: GoogleInputProps) => (
-        <View className={`flex flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}>
-            <Text>Search</Text>
-        </View>
-      
-        
-    
-);
+
+const GoogleTextInput = ({
+  icon,
+  initialLocation,
+  containerStyle,
+  textInputBackgroundColor,
+  handlePress,
+}: GoogleInputProps) => {
+  return (
+    <TouchableOpacity 
+   
+      className={`flex flex-row items-center justify-between px-4 py-3 rounded-xl z-50 mb-5 ${containerStyle}`}
+    >
+      {icon && (
+        <Image 
+          source={typeof icon === 'string' ? { uri: icon } : icon} 
+          className="w-5 h-5 mr-3" 
+          resizeMode="contain" 
+        />
+      )}
+      <Text className="flex-1 text-neutral-500">
+        Search
+      </Text>
+    </TouchableOpacity>
+  );
+};
 
 export default GoogleTextInput;
